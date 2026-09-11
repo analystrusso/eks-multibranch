459e62c7e9b8f11932a8e08a8b6d4ea108444b9d
Adam Russo <adam.jordan.russo@gmail.com>
2026-09-11 10:48:12 -0400

    added conditional when

--- script.groovy
+++ script.groovy
@@ -1,10 +1,10 @@
 def buildJar() {
-    echo "executing pipeline for branch $BRANCH_NAME"
     echo 'building the application...'
     sh 'mvn package'
 }
 
 def buildImage() {
+    echo "executing pipeline for branch $BRANCH_NAME"
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t analystrusso/twn-bootcamp-repo:jma-2.0 .'
